57e6c916683deb9df10761d1fccfd5cf8dbf4281
ryhszk <ryh.szk.cmnty@gmail.com>
2018-11-23 21:13:04 +0900

    Remove comments and, change a print msg

--- kpanic/null-pointer.c
+++ kpanic/null-pointer.c
@@ -1,15 +1,11 @@
-/*
- *  null-pointer.c - A not so simple kernel module to crash kernel.
- */
-
-#include <linux/module.h>    /* Needed by all modules */
-#include <linux/kernel.h>    /* Needed for KERN_INFO */
+#include <linux/module.h>
+#include <linux/kernel.h>
 
 char *p=NULL;
 
 int init_module(void)
 {
-    printk(KERN_INFO "We is gonna KABOOM now!\n");
+    printk(KERN_INFO "call the init_module\n");
 
     *p = 1;
     return 0;
@@ -17,5 +13,5 @@ int init_module(void)
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO "Goodbye world.\n");
+    printk(KERN_INFO "call the cleannup_module\n");
 }
